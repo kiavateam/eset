6baf947ae5b4d6559086e031adc9326c137198cc
mohammadhasanmonsefirad <mizbanivps@gmail.com>
2017-03-22 13:29:10 +0000

    Update bot.lua

--- cli/bot/bot.lua
+++ cli/bot/bot.lua
@@ -875,7 +875,7 @@ end
             end
           tdcli_function({
       ID = "GetInlineQueryResults",
-      bot_user_id_ = 379506376,
+      bot_user_id_ = 357898384,
       chat_id_ = msg.chat_id_,
       user_location_ = {
         ID = "Location",
